e119af2c21f68cf9c4b520790f9f7379a8b23bfe
walid <idrisswalid10@gmail.com>
2024-04-19 22:02:56 +0100

    com

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import million from "million/compiler";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -11,7 +10,4 @@ const nextConfig = {
   },
 };
 
-const millionConfig = {
-  auto: true,// if you're using RSC: auto: { rsc: true },
-};
-export default million.next(nextConfig);
+export default nextConfig;
