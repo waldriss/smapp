10b8f546eab669c9bf1698f4862c4b1953d26496
walid <idrisswalid10@gmail.com>
2024-04-19 21:33:21 +0100

    comm

--- next.config.mjs
+++ next.config.mjs
@@ -14,5 +14,4 @@ const nextConfig = {
 const millionConfig = {
   auto: true,// if you're using RSC: auto: { rsc: true },
 };
- 
-export default million.next(nextConfig, millionConfig);
+export default million.next(nextConfig);
